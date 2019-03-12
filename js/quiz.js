979a47c90171102c5860f017ee9dc710b5810665
Kyle Monahan <kyle.monahan2000@gmail.com>
2019-03-12 13:53:22 -0500

    got quiz working

--- js/quiz.js
+++ js/quiz.js
@@ -1,61 +1,36 @@
 'use strict';
 
 var myName = prompt('Is my name Skylar?');
-if(myName.toUpperCase === 'YES'){
-  console.log(true);
+if(myName.toUpperCase() === 'YES' || myName.toUpperCase() === 'Y'){
+  console.log('true');
 }else{
-  console.log(false);
-}
-if(myName.toUpperCase === 'Y'){
-  console.log(true);
-}else{
-  console.log(false);
+  console.log('false');
 }
 
-var myAge = prompt('AM I 18 years old?');
-if(myAge.toUpperCase === 'YES'){
-  console.log(true);
+var myAge = prompt('Am I 18 years old?');
+if(myAge.toUpperCase() === 'YES' || myAge.toUpperCase() === 'Y'){
+  console.log('true');
 }else{
-  console.log(false);
-}
-if(myAge.toUpperCase === 'Y'){
-  console.log(true);
-}else{
-  console.log(false);
+  console.log('false');
 }
 
 var favColor = prompt('Is my favorite color pink?');
-if(favColor.toUpperCase === 'YES'){
-  console.log(true);
+if(favColor.toUpperCase() === 'YES' || favColor.toUpperCase() === 'Y'){
+  console.log('true');
 }else{
-  console.log(false);
-}
-if(favColor.toUpperCase === 'Y'){
-  console.log(true);
-}else{
-  console.log(false);
+  console.log('false');
 }
 
 var myHeight = prompt('Am I 5 foot 6 inches?');
-if(myHeight.toUpperCase === 'NO'){
-  console.log(true);
+if(myHeight.toUpperCase() === 'NO' || myHeight.toUpperCase() === 'N'){
+  console.log('true');
 }else{
-  console.log(false);
-}
-if(myHeight.toUpperCase === 'N'){
-  console.log(true);
-}else{
-  console.log(false);
+  console.log('false');
 }
 
 var meFunny = prompt('Am I funny?');
-if(meFunny.toUpperCase === 'YES'){
-  console.log(true);
+if(meFunny.toUpperCase() === 'YES' || meFunny.toUpperCase() === 'Y'){
+  console.log('true');
 }else{
-  console.log(false);
-}
-if(meFunny.toUpperCase === 'Y'){
-  console.log(true);
-}else{
-  console.log(false);
+  console.log('false');
 }

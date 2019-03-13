7332e1c68b3327f134c456ed770c6060769f771a
Kyle Monahan <kyle.monahan2000@gmail.com>
2019-03-13 15:03:49 -0500

    added for and while loops on new questions

--- js/app.js
+++ js/app.js
@@ -1,35 +1,14 @@
 'use strict';
-var programmer = promptRequired('What is your name?');
+var programmer = prompt('What is your name?');
 console.log(programmer + ' is their name.');
-var game = promptRequired('What is your favorite video game?');
+var game = prompt('What is your favorite video game?');
 console.log(game + ' is their favorite video game.');
-var food = promptRequired('What is your favorite food?');
+var food = prompt('What is your favorite food?');
 console.log(food + ' is their favorite food.');
-var music = promptRequired('What music genre do you enjoy?');
+var music = prompt('What music genre do you enjoy?');
 console.log(music + ' is their favorite music.');
 var answers = ('Salutations ' + programmer + ', ' + game + ' sounds like an interesting game, ' + food + ' sounds delicious, and ' + music + ' sounds good to me!');
 alert('Salutations ' + programmer + ', ' + game + ' sounds like an interesting game, ' + food + ' sounds delicious, and ' + music + ' sounds good to me!');
-
-function answerIsValid(answer) {
-  if (!answer) {
-    return false;
-  }
-  answer = answer.trim();
-  if (!answer) {
-    return false;
-  }
-  if (answer === 'null' || answer === 'undefined') {
-    return false;
-  }
-  return true;
-}
-function promptRequired(message) {
-  var result = prompt(message);
-  while (!answerIsValid(result)) {
-    result = prompt('REQUIRED: ' + message);
-  }
-  return result;
-}
 
 var results = document.getElementById('results');
 results.innerHTML=answers;
